cd4e6a97c69644630666bf0c4afeb1dd970354ea
Berdnikov Alexey <aberdnikov@gmail.com>
2017-04-20 09:05:36 +0500

    Update init.php

--- init.php
+++ init.php
@@ -1,6 +1,5 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-drag-and-drop-lists')
     ->usePackage('larakit/sf-angular')
-    ->ngModule('dndLists')
     ->setSourceDir('public')
     ->jsPackage('angular-drag-and-drop-lists.min.js');
